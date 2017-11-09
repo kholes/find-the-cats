3e391b1256a067fd14cafaed21015f7a9d2de4fc
kholes <cholis.elbazar@gmail.com>
2017-11-09 11:46:39 +0700

    App pancet

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-// const Home = (Home) => import('@/components/Home')
-// const Post = (Post) => import('@/components/Post')
-// const Detail = (Detail) => import('@/components/Detail')
+const Home = (Home) => import('@/components/Home')
+const Post = (Post) => import('@/components/Post')
+const Detail = (Detail) => import('@/components/Detail')
 
 Vue.use(Router)
 
